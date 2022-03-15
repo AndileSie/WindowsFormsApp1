eb5680fa64f58ddf2471c5bfa23900395332afeb
Andile Simon Banda <s224097083@mandela.ac.za>
2022-03-15 14:48:06 +0200

    ..

--- WindowsFormsApp1/frmFualt.cs
+++ WindowsFormsApp1/frmFualt.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void frmFualt_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- WindowsFormsApp1/frmStudent.cs
+++ WindowsFormsApp1/frmStudent.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void frmStudent_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
