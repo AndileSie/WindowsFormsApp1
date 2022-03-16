de53cba11f82ebd7bc124110059bd87c02a9ae74
Mbali Entle Mpendu <mbalientlempendu02@gmail.com>
2022-03-16 16:12:01 +0200

    ass

--- WindowsFormsApp1/frmMenu.Designer.cs
+++ WindowsFormsApp1/frmMenu.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 404);
+            this.ClientSize = new System.Drawing.Size(682, 434);
             this.Name = "frmMenu";
             this.Text = "frmMenu";
             this.ResumeLayout(false);
