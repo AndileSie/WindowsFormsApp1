3b9470c58007cd12476c0e6e964c6dbac1d371bb
Daylin Goldman <s224024981@mandela.ac.za>
2022-03-17 10:22:10 +0200

    This is for the Student User code..Im done

--- WindowsFormsApp1/frmStudent.Designer.cs
+++ WindowsFormsApp1/frmStudent.Designer.cs
@@ -28,20 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbYearOfStudy = new System.Windows.Forms.ComboBox();
+            this.lblYearOfStudy = new System.Windows.Forms.Label();
+            this.btnGoBack = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstDisplayItems = new System.Windows.Forms.ListBox();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.dtpDOB = new System.Windows.Forms.DateTimePicker();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbYearOfStudy);
+            this.groupBox1.Controls.Add(this.lblYearOfStudy);
+            this.groupBox1.Controls.Add(this.btnGoBack);
+            this.groupBox1.Controls.Add(this.btnList);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.lstDisplayItems);
+            this.groupBox1.Controls.Add(this.cmbRole);
+            this.groupBox1.Controls.Add(this.dtpDOB);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtUserID);
+            this.groupBox1.Controls.Add(this.lblRole);
+            this.groupBox1.Controls.Add(this.lblDOB);
+            this.groupBox1.Controls.Add(this.lblSurname);
+            this.groupBox1.Controls.Add(this.lblName);
+            this.groupBox1.Controls.Add(this.lblUserID);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(17, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(766, 447);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Information";
+            // 
+            // cmbYearOfStudy
+            // 
+            this.cmbYearOfStudy.FormattingEnabled = true;
+            this.cmbYearOfStudy.Location = new System.Drawing.Point(188, 226);
+            this.cmbYearOfStudy.Name = "cmbYearOfStudy";
+            this.cmbYearOfStudy.Size = new System.Drawing.Size(198, 28);
+            this.cmbYearOfStudy.TabIndex = 15;
+            this.cmbYearOfStudy.SelectedIndexChanged += new System.EventHandler(this.cmbYearOfStudy_SelectedIndexChanged);
+            // 
+            // lblYearOfStudy
+            // 
+            this.lblYearOfStudy.AutoSize = true;
+            this.lblYearOfStudy.Location = new System.Drawing.Point(5, 226);
+            this.lblYearOfStudy.Name = "lblYearOfStudy";
+            this.lblYearOfStudy.Size = new System.Drawing.Size(106, 20);
+            this.lblYearOfStudy.TabIndex = 14;
+            this.lblYearOfStudy.Text = "Year of Study";
+            // 
+            // btnGoBack
+            // 
+            this.btnGoBack.Location = new System.Drawing.Point(424, 108);
+            this.btnGoBack.Name = "btnGoBack";
+            this.btnGoBack.Size = new System.Drawing.Size(115, 37);
+            this.btnGoBack.TabIndex = 13;
+            this.btnGoBack.Text = "Go Back";
+            this.btnGoBack.UseVisualStyleBackColor = true;
+            this.btnGoBack.Click += new System.EventHandler(this.btnGoBack_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(424, 65);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(115, 37);
+            this.btnList.TabIndex = 12;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(424, 22);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 37);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstDisplayItems
+            // 
+            this.lstDisplayItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstDisplayItems.FormattingEnabled = true;
+            this.lstDisplayItems.ItemHeight = 16;
+            this.lstDisplayItems.Location = new System.Drawing.Point(12, 268);
+            this.lstDisplayItems.Name = "lstDisplayItems";
+            this.lstDisplayItems.Size = new System.Drawing.Size(754, 132);
+            this.lstDisplayItems.TabIndex = 10;
+            this.lstDisplayItems.SelectedIndexChanged += new System.EventHandler(this.lstDisplayItems_SelectedIndexChanged);
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(188, 187);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(198, 28);
+            this.cmbRole.TabIndex = 9;
+            this.cmbRole.SelectedIndexChanged += new System.EventHandler(this.cmbRole_SelectedIndexChanged);
+            // 
+            // dtpDOB
+            // 
+            this.dtpDOB.Location = new System.Drawing.Point(187, 146);
+            this.dtpDOB.Name = "dtpDOB";
+            this.dtpDOB.Size = new System.Drawing.Size(199, 26);
+            this.dtpDOB.TabIndex = 8;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(188, 104);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(198, 26);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(188, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(198, 26);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(188, 29);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(198, 26);
+            this.txtUserID.TabIndex = 5;
+            this.txtUserID.TextChanged += new System.EventHandler(this.txtUserID_TextChanged);
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRole.Location = new System.Drawing.Point(6, 187);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(43, 18);
+            this.lblRole.TabIndex = 4;
+            this.lblRole.Text = "Role:";
+            // 
+            // lblDOB
+            // 
+            this.lblDOB.AutoSize = true;
+            this.lblDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDOB.Location = new System.Drawing.Point(6, 146);
+            this.lblDOB.Name = "lblDOB";
+            this.lblDOB.Size = new System.Drawing.Size(97, 18);
+            this.lblDOB.TabIndex = 3;
+            this.lblDOB.Text = "Date Of Birth:";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSurname.Location = new System.Drawing.Point(6, 104);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(108, 18);
+            this.lblSurname.TabIndex = 2;
+            this.lblSurname.Text = "User Surname:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(6, 66);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(88, 18);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "User Name:";
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserID.Location = new System.Drawing.Point(6, 29);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(62, 18);
+            this.lblUserID.TabIndex = 0;
+            this.lblUserID.Text = "User ID:";
             // 
             // frmStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmStudent";
             this.Text = "frmStudent";
             this.Load += new System.EventHandler(this.frmStudent_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbYearOfStudy;
+        private System.Windows.Forms.Label lblYearOfStudy;
+        private System.Windows.Forms.Button btnGoBack;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstDisplayItems;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.DateTimePicker dtpDOB;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.Label lblDOB;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblUserID;
     }
 }
--- WindowsFormsApp1/frmStudent.cs
+++ WindowsFormsApp1/frmStudent.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -16,8 +17,114 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
+        int idNum = 1000001;
         private void frmStudent_Load(object sender, EventArgs e)
+        {
+            txtUserID.Enabled = false;
+            txtUserID.Text = idNum.ToString();
+            cmbRole.Items.Add("Student");
+            cmbRole.Items.Add("Intern");
+            cmbYearOfStudy.Items.Add("First Year");
+            cmbYearOfStudy.Items.Add("Second Year");
+            cmbYearOfStudy.Items.Add("Third Year");
+            lblYearOfStudy.Visible = false;
+            cmbYearOfStudy.Visible = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cmbRole.SelectedItem.ToString() == "Student")
+            {
+                StreamWriter student = new StreamWriter("Student.txt", true);
+
+                student.WriteLine(txtUserID.Text + "#" + txtName.Text + "#" + txtSurname.Text + "#" + dtpDOB.Text + "#" + cmbRole.SelectedItem.ToString() + "#" + cmbYearOfStudy.SelectedItem.ToString());
+
+                student.Close();
+            }
+            else if (cmbRole.SelectedItem.ToString() == "Intern")
+            {
+                StreamWriter intern = new StreamWriter("Intern.txt", true);
+
+                intern.WriteLine(txtUserID.Text + "#" + txtName.Text + "#" + txtSurname.Text + "#" + dtpDOB.Text + "#" + cmbRole.SelectedItem.ToString());
+
+                intern.Close();
+            }
+            txtUserID.Clear();
+            txtName.Clear();
+            txtSurname.Clear();
+            cmbRole.ResetText();
+            cmbYearOfStudy.ResetText();
+            MessageBox.Show("Record Successfully Added!");
+
+            idNum++;
+            txtUserID.Text = idNum.ToString();
+
+            lblYearOfStudy.Visible = false;
+            cmbYearOfStudy.Visible = false;
+
+            lstDisplayItems.Items.Clear();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lstDisplayItems.Items.Add("User ID" + "\t" + "User Name" + "\t" + "User Surname" + "\t" + "Date of Birth" + "\t\t" + "Role" + "\t" + "Year of Study");
+
+            StreamReader student = new StreamReader("Student.txt");
+            string line = "";
+            string[] record;
+
+            while ((line = student.ReadLine()) != null)
+            {
+                record = line.Split('#');
+                lstDisplayItems.Items.Add(record[0] + "\t" + record[1] + "\t\t" + record[2] + "\t\t" + record[3] + "\t" + record[4] + "\t" + record[5]);
+            }
+            student.Close();
+
+            StreamReader intern = new StreamReader("Intern.txt");
+            string row = "";
+            string[] field;
+
+            while ((row = intern.ReadLine()) != null)
+            {
+                field = row.Split('#');
+                lstDisplayItems.Items.Add(field[0] + "\t" + field[1] + "\t\t" + field[2] + "\t\t" + field[3] + "\t" + field[4]);
+            }
+            intern.Close();
+        }
+
+        private void btnGoBack_Click(object sender, EventArgs e)
+        {
+            frmMenu menu = new frmMenu();
+            menu.Show();
+            this.Hide();
+            
+        }
+
+        private void txtUserID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbRole_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbRole.SelectedItem.ToString() == "Student")
+            {
+                lblYearOfStudy.Visible = true;
+                cmbYearOfStudy.Visible = true;
+            }
+            else if (cmbRole.SelectedItem.ToString() == "Intern")
+            {
+                lblYearOfStudy.Visible = false;
+                cmbYearOfStudy.Visible = false;
+            }
+        }
+
+        private void cmbYearOfStudy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstDisplayItems_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
